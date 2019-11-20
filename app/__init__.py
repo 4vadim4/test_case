daa38a375493b4c7857ad216b6812a599a94ab1c
Vadim B <4vadim4@gmail.com>
2019-11-20 10:19:01 +0300

    add dir if it's absent

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
@@ -8,5 +9,9 @@ app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+if not os.path.isdir('./static/photos'):
+    os.mkdir('static')
+    os.mkdir('static/photos')
 
 from app import routes, models
